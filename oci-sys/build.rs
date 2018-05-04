d53d4a272655ecd7adf49748dddb2475950eb440
pgab <paul.gabriel@giga-infosystems.com>
2018-05-04 15:58:06 +0200

    linking OCI rather than OCCI

--- oci-sys/build.rs
+++ oci-sys/build.rs
@@ -1,10 +1,13 @@
 use std::env;
 
 fn main() {
-    if let Ok(lib_dir) = env::var("OCCI_LIB_DIR") {
+    if let Ok(lib_dir) = env::var("OCI_LIB_DIR") {
         println!("cargo:rustc-link-search=native={}", lib_dir);
     } else {
-        println!("Please set OCCI_LIB_DIR to build oci-sys");
+        println!("Please set OCI_LIB_DIR to build oci-sys");
     }
+    //println!("cargo:rustc-link-lib={}", "occi");
+    //println!("cargo:rustc-link-lib={}", "ociei");
+    println!("cargo:rustc-link-lib={}", "clntsh");
 }
 

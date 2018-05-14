2e81ad497164b18c71fc705b441ba2c5e98c7c85
pgab <paul.gabriel@giga-infosystems.com>
2018-05-14 21:57:02 +0200

    splitting creation of __DIESEL_SCHEMA_MIGRATIONS table in separate commands and execute them. since CREATE IF NOT exists an SP is being used

--- src/oracle/connection/mod.rs
+++ src/oracle/connection/mod.rs
@@ -33,11 +33,28 @@ pub struct OciConnection {
 unsafe impl Send for OciConnection {}
 
 impl MigrationConnection for OciConnection {
-    const CREATE_MIGRATIONS_TABLE: &'static str =
-         "CREATE TABLE \"__DIESEL_SCHEMA_MIGRATIONS\" (\
+    const CREATE_MIGRATIONS_FUNCTION: &'static str =
+        "create or replace procedure create_if_not_exists(input_sql varchar2) \
+        as \
+        begin \
+            execute immediate input_sql; \
+            exception \
+                when others then \
+                if sqlcode = -955 then \
+                    NULL; \
+                else \
+                    raise; \
+            end if; \
+        end; \n ";
+
+    const CREATE_MIGRATIONS_TABLE: &'static str = "
+    declare \
+    begin \
+    create_if_not_exists('CREATE TABLE \"__DIESEL_SCHEMA_MIGRATIONS\" (\
          \"VERSION\" VARCHAR2(50) PRIMARY KEY NOT NULL,\
          \"RUN_ON\" TIMESTAMP with time zone DEFAULT sysdate not null\
-         )";
+         )'); \
+        end; \n";
 }
 
 impl SimpleConnection for OciConnection {

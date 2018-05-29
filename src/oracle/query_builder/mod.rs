3c23ad6414230d366de4def04eac778dd13f7dd2
pgab <paul.gabriel@giga-infosystems.com>
2018-05-29 15:52:00 +0200

    moved code to proper location

--- src/oracle/query_builder/mod.rs
+++ src/oracle/query_builder/mod.rs
@@ -46,47 +46,5 @@ impl QueryBuilder<Oracle> for OciQueryBuilder {
     }
 }
 
-//#![feature(specialization)]
 
-//use diesel::insertable::Insertable;
-//use diesel::query_builder::insert_statement::InsertStatement;
-//use diesel::query_dsl::methods::ExecuteDsl;
-//use diesel::result::QueryResult;
-//use diesel::query_builder::QueryFragment;
 
-// TODO:
-//    error[E0210]: type parameter `T` must be used as the type parameter for some local type (e.g. `MyStruct<T>`); only traits defined in the current crate can be implemented for a type parameter
-//    --> src/oracle/query_builder.rs:54:1
-//    |
-//    54 | / impl<'a, T, U, Op> ExecuteDsl<OciConnection> for InsertStatement<T, &'a [U], Op>
-//    55 | |     where
-//    56 | |         &'a U: Insertable<T>,
-//    57 | |         InsertStatement<T, <&'a U as Insertable<T>>::Values, Op>: QueryFragment<Oracle>,
-//    ...  |
-//    75 | |     }
-//    76 | | }
-//    | |_^
-////#[deprecated(since = "1.2.0", note = "Use `<&'a [U] as Insertable<T>>::Values` instead")]
-//impl<'a, T, U, Op> ExecuteDsl<OciConnection> for InsertStatement<T, &'a [U], Op>
-//    where
-//        &'a U: Insertable<T>,
-//        InsertStatement<T, <&'a U as Insertable<T>>::Values, Op>: QueryFragment<Oracle>,
-//        T: Copy,
-//        Op: Copy,
-//{
-//    fn execute(query: Self, conn: &OciConnection) -> QueryResult<usize> {
-//        use diesel::connection::Connection;
-//        conn.transaction(|| {
-//            let mut result = 0;
-//            for record in query.records {
-//                result += InsertStatement::new(
-//                    query.target,
-//                    record.values(),
-//                    query.operator,
-//                    query.returning,
-//                ).execute(conn)?;
-//            }
-//            Ok(result)
-//        })
-//    }
-//}

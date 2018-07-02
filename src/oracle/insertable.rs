fde1bb3e1871777255f226da8e211bc4e77e747e
pgab <paul.gabriel@giga-infosystems.com>
2018-07-02 10:26:37 +0200

    added clarification about a playground test and why it can't be used, but needs to be used for oracle

--- src/oracle/insertable.rs
+++ src/oracle/insertable.rs
@@ -1,5 +1,28 @@
 #![feature(specialization)]
 
+// This fails to compile since we cannot specialize QueryFragement here, but we would need to this
+// since Oracle demands a different syntax for batch inserts, ala `insert into t(a,b) select (1,2)
+// from dual union all select (4,5) from dual`
+// once https://github.com/rust-lang/rfcs/pull/2451 has been accepted and
+// implemented in Rust (or diesel changed something else) we can go forward with this
+// otherwise we would need to implement an own SQL parser
+
+// error[E0119]: conflicting implementations of trait `diesel::query_builder::QueryFragment<oracle::backend::Oracle>` for type `diesel::insertable::BatchInsert<'_, _, _>`:
+//  --> src/oracle/insertable.rs:21:1
+//   |
+//21 | / impl<'a, T, Tab, Inner> QueryFragment<Oracle> for BatchInsert<'a, T, Tab>
+//22 | | where
+//23 | |     &'a T: Insertable<Tab, Values = ValuesClause<Inner, Tab>>,
+//24 | |     ValuesClause<Inner, Tab>: QueryFragment<Oracle>,
+//...  |
+//38 | |     }
+//39 | | }
+//   | |_^
+//   |
+//   = note: conflicting implementation in crate `diesel`:
+//           - impl<'a, T, Tab, Inner, DB> diesel::query_builder::QueryFragment<DB> for diesel::insertable::BatchInsert<'a, T, Tab>
+//             where <&'a T as diesel::Insertable<Tab>>::Values == diesel::query_builder::ValuesClause<Inner, Tab>, DB: diesel::backend::Backend, DB: diesel::backend::SupportsDefaultKeyword, &'a T: diesel::Insertable<Tab>, diesel::query_builder::ValuesClause<Inner, Tab>: diesel::query_builder::QueryFragment<DB>, Inner: diesel::query_builder::QueryFragment<DB>;
+
 use diesel::query_builder::QueryFragment;
 use diesel::query_builder::ValuesClause;
 

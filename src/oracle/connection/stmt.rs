87e929623aff4b7c2e061d2d1991d7d04bdca58a
pgab <paul.gabriel@giga-infosystems.com>
2018-07-13 15:05:01 +0200

    yet again adjusting byte length to password (may I count wrong?)

--- src/oracle/connection/stmt.rs
+++ src/oracle/connection/stmt.rs
@@ -283,7 +283,7 @@ impl Statement {
                 ffi::SQLT_BLOB => {
                     tpe = ffi::SQLT_BIN;
                     tpe_size=310;
-                    tpe_size=88;
+                    tpe_size=89;
                 }
                 _ => {
                     return Err(Error::DatabaseError(

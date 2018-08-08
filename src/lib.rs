8d401e469f9a133ac47d34a19d1774bea56ac93f
pgab <paul.gabriel@giga-infosystems.com>
2018-08-08 16:05:26 +0200

    added use of crate `num` since tests use it

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,14 @@ extern crate diesel;
 extern crate bigdecimal;
 extern crate byteorder;
 extern crate libc;
+#[macro_use]
 extern crate log;
 extern crate oci_sys;
 pub mod oracle;
+
+extern crate num;
+#[macro_use]
+extern crate num_derive;
 
 #[cfg(test)]
 mod test;

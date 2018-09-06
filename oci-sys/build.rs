3bfca727e5ec340afc96269c70e7c948b0a87eef
Daniel Buse <daniel.buse@giga-infosystems.com>
2018-09-06 11:51:18 +0200

    panic if OCI_LIB_DIR isn't set

--- oci-sys/build.rs
+++ oci-sys/build.rs
@@ -10,7 +10,7 @@ fn main() {
     if let Ok(lib_dir) = env::var("OCI_LIB_DIR") {
         println!("cargo:rustc-link-search=native={}", lib_dir);
     } else {
-        println!("Please set OCI_LIB_DIR to build oci-sys");
+        panic!("Please set OCI_LIB_DIR to build oci-sys");
     }
     println!("cargo:rustc-link-lib={}", OCI_LIB);
 }

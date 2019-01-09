b2c7e1b7c32209baa9d6192880d0e5079fa7b088
pgab <paul.gabriel@giga-infosystems.com>
2019-01-09 10:26:37 +0100

    fixing exists query, using `SELECT CASE WHEN EXISTS (subquery) THEN 1 ELSE 0 END FROM DUAL`

--- src/oracle/query_builder/exists.rs
+++ src/oracle/query_builder/exists.rs
@@ -9,9 +9,9 @@ where
     T: QueryFragment<Oracle>,
 {
     fn walk_ast(&self, mut out: AstPass<Oracle>) -> QueryResult<()> {
-        out.push_sql("1 FROM DUAL WHERE EXISTS (");
+        out.push_sql("CASE WHEN EXISTS (");
         self.0.walk_ast(out.reborrow())?;
-        out.push_sql(")");
+        out.push_sql(") THEN 1 ELSE 0 END FROM DUAL");
         Ok(())
     }
 }

0fa0f0c291cbd62957afa8fad73b97793dcfc26b
pgab <paul.gabriel@giga-infosystems.com>
2019-08-23 02:09:41 +0200

    outputting directly to console rather than `LOGFILE`

--- .travis/ora/wget_12c.sh
+++ .travis/ora/wget_12c.sh
@@ -23,16 +23,16 @@ LOGFILE=$LOGDIR/wgetlog-$(date +%m-%d-%y-%H:%M).log
 echo "Wget version info: 
 ------------------------------
 $($WGET -V) 
-------------------------------" > "$LOGFILE" 2>&1 
+------------------------------"  
 
 # Location of cookie file 
-COOKIE_FILE=$(mktemp -t wget_sh_XXXXXX) >> "$LOGFILE" 2>&1 
+COOKIE_FILE=$(mktemp -t wget_sh_XXXXXX)
 if [ $? -ne 0 ] || [ -z "$COOKIE_FILE" ] 
 then 
  echo "Temporary cookie file creation failed. See $LOGFILE for more details." |  tee -a "$LOGFILE" 
  exit 1 
 fi 
-echo "Created temporary cookie file $COOKIE_FILE" >> "$LOGFILE" 
+echo "Created temporary cookie file $COOKIE_FILE"
 
 # Output directory and file
 OUTPUT_DIR=.travis/ora/12.2.0.1
@@ -43,7 +43,7 @@ OUTPUT_DIR=.travis/ora/12.2.0.1
 # The following command to authenticate uses HTTPS. This will work only if the wget in the environment
 # where this script will be executed was compiled with OpenSSL.
 # 
- $WGET  --secure-protocol=auto --save-cookies="$COOKIE_FILE" --keep-session-cookies --http-user "$SSO_USERNAME" --http-password "$SSO_PWD"  "https://edelivery.oracle.com/osdc/cliauth" -O /dev/null 2>> "$LOGFILE"
+ $WGET  --secure-protocol=auto --save-cookies="$COOKIE_FILE" --keep-session-cookies --http-user "$SSO_USERNAME" --http-password "$SSO_PWD"  "https://edelivery.oracle.com/osdc/cliauth" -O /dev/null
 
 # Verify if authentication is successful 
 if [ $? -ne 0 ] 
@@ -51,8 +51,8 @@ then
  echo "Authentication failed with the given credentials." | tee -a "$LOGFILE"
  echo "Please check logfile: $LOGFILE for more details." 
 else
- echo "Authentication is successful. Proceeding with downloads..." >> "$LOGFILE" 
- $WGET --load-cookies="$COOKIE_FILE" "https://edelivery.oracle.com/osdc/softwareDownload?fileName=V839960-01.zip&token=S1hMME9sUWkwWitTaDFpMnorbW56dyE6OiFmaWxlSWQ9OTI3NjM5MjgmZmlsZVNldENpZD03ODQ4MzImcmVsZWFzZUNpZHM9MjQ2MTg5JnBsYXRmb3JtQ2lkcz0zNSZkb3dubG9hZFR5cGU9OTU3NjQmYWdyZWVtZW50SWQ9NTc4MzIzNSZlbWFpbEFkZHJlc3M9cGF1bEBnaWdhLWluZm9zeXN0ZW1zLmNvbSZ1c2VyTmFtZT1FUEQtUEFVTEBHSUdBLUlORk9TWVNURU1TLkNPTSZpcEFkZHJlc3M9MmEwMjoyNDUwOjExOTk6YmY0OjgxZjk6OTU5ZDoyY2VhOjc5M2MmdXNlckFnZW50PU1vemlsbGEvNS4wIChYMTE7IExpbnV4IHg4Nl82NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzc2LjAuMzgwOS4xMDAgU2FmYXJpLzUzNy4zNiZjb3VudHJ5Q29kZT1ERSZkbHBDaWRzPTg3NDAwNA" -O "$OUTPUT_DIR/linuxx64_12201_database.zip" >> "$LOGFILE" 2>&1 
+ echo "Authentication is successful. Proceeding with downloads..." 
+ $WGET --load-cookies="$COOKIE_FILE" "https://edelivery.oracle.com/osdc/softwareDownload?fileName=V839960-01.zip&token=S1hMME9sUWkwWitTaDFpMnorbW56dyE6OiFmaWxlSWQ9OTI3NjM5MjgmZmlsZVNldENpZD03ODQ4MzImcmVsZWFzZUNpZHM9MjQ2MTg5JnBsYXRmb3JtQ2lkcz0zNSZkb3dubG9hZFR5cGU9OTU3NjQmYWdyZWVtZW50SWQ9NTc4MzIzNSZlbWFpbEFkZHJlc3M9cGF1bEBnaWdhLWluZm9zeXN0ZW1zLmNvbSZ1c2VyTmFtZT1FUEQtUEFVTEBHSUdBLUlORk9TWVNURU1TLkNPTSZpcEFkZHJlc3M9MmEwMjoyNDUwOjExOTk6YmY0OjgxZjk6OTU5ZDoyY2VhOjc5M2MmdXNlckFnZW50PU1vemlsbGEvNS4wIChYMTE7IExpbnV4IHg4Nl82NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzc2LjAuMzgwOS4xMDAgU2FmYXJpLzUzNy4zNiZjb3VudHJ5Q29kZT1ERSZkbHBDaWRzPTg3NDAwNA" -O "$OUTPUT_DIR/linuxx64_12201_database.zip" 
 fi 
 
 # Cleanup

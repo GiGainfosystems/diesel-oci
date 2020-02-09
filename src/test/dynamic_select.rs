e04cfb9183f05a685f176fb79d908c06f75e2003
pgab <paul.gabriel@giga-infosystems.com>
2020-02-09 23:47:56 +0100

    fixing tests, since oracle converts everything to uppercase we need to take care of that as well (or probably at a different place?)

--- src/test/dynamic_select.rs
+++ src/test/dynamic_select.rs
@@ -59,40 +59,40 @@ SELECT * FROM DUAL")
         users.select(select).load(&connection).unwrap();
 
     assert_eq!(
-        actual_data[0]["name"],
+        actual_data[0]["NAME"],
         MyDynamicValue::String("Sean".into())
     );
     assert_eq!(
         actual_data[0][1],
         NamedField {
-            name: "name".into(),
+            name: "NAME".into(),
             value: MyDynamicValue::String("Sean".into())
         }
     );
     assert_eq!(
-        actual_data[1]["name"],
+        actual_data[1]["NAME"],
         MyDynamicValue::String("Tess".into())
     );
     assert_eq!(
         actual_data[1][1],
         NamedField {
-            name: "name".into(),
+            name: "NAME".into(),
             value: MyDynamicValue::String("Tess".into())
         }
     );
-    assert_eq!(actual_data[0]["hair_color"], MyDynamicValue::Null);
+    assert_eq!(actual_data[0]["HAIR_COLOR"], MyDynamicValue::Null);
     assert_eq!(
         actual_data[0][2],
         NamedField {
-            name: "hair_color".into(),
+            name: "HAIR_COLOR".into(),
             value: MyDynamicValue::Null
         }
     );
-    assert_eq!(actual_data[1]["hair_color"], MyDynamicValue::Null);
+    assert_eq!(actual_data[1]["HAIR_COLOR"], MyDynamicValue::Null);
     assert_eq!(
         actual_data[1][2],
         NamedField {
-            name: "hair_color".into(),
+            name: "HAIR_COLOR".into(),
             value: MyDynamicValue::Null
         }
     );
